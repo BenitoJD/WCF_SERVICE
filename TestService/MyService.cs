7f3a96c50e6bc639204c1c96305faa2ce047c31e
benito JD <benito.josede@gmail.com>
2024-12-03 14:37:15 +0530

    Added GetData

---
+++ TestService/MyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+     class MyService : IMyService
+    {
+        public string GetData()
+        {
+            return "www.Benify.com";
+        }
+    }
+}
