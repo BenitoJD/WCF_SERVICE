9201ba0f14253a4431dacc0b78bf83b154ef9617
benito JD <benito.josede@gmail.com>
2024-12-03 14:56:23 +0530

    Passing various kind of parameters

--- TestService/MyService.cs
+++ TestService/MyService.cs
@@ -13,5 +13,38 @@ namespace TestService
         {
             return "www.Benify.com";
         }
+
+        public int GetMax(int[] ar)
+        {
+            int Max = ar[0];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] > Max)
+                {
+                    Max = ar[i];
+                }
+            }     
+            return Max;
+        }
+
+        public string GetMessage(string Name)
+        {
+            return "Hello" + Name;    
+        }
+
+        public string GetResult(int Sid, string SName, int M1, int M2, int M3)
+        {
+            double Avg = (M1 + M2 + M3)/ 3.0;
+            if (Avg < 35)
+                return "Fail";
+            else
+                return "Pass";
+        }
+
+        public int[] GetSorted(int[] ar)
+        {
+            Array.Sort(ar);
+            return ar;
+        }
     }
 }
