82c207b8fd5c58ed8a51c5866d0ff8ffe4004f97
benito JD <benito.josede@gmail.com>
2024-12-07 15:40:08 +0530

    adder consumers

---
+++ MyClient/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyClient.MyServiceReference;
+
+namespace MyClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyServiceClient myServiceClient = new MyServiceClient();
+            DropDownList1.DataSource = myServiceClient.GetAllCountry();
+
+            DropDownList1.DataValueField = "CountryId";
+            DropDownList1.DataTextField = "CountryName";
+            DropDownList1.DataBind();
+        }
+    }
+}
